d06ed80a8b6777a69a59b77603df9b44ccee6b42
Alexandrediasss <alexandredml2005@gmail.com>
2026-05-14 17:04:50 -0400

    Add steps content

---
+++ EndoCare/View/CreateAccount/CreateAccountForms.swift
@@ -0,0 +1,63 @@
+//
+//  CreateAccountForms.swift
+//  EndoCare
+//
+//  Created by Academy on 14/05/26.
+//
+
+import SwiftUI
+
+struct StepContent {
+    let label1: String
+    let label2: String
+    let label3: String
+    let text1: String
+    let text2: String
+    let text3: String
+}
+
+struct CreateAccountForms: View {
+    @State private var steps : String = "step1"
+    
+    let step1Content = StepContent(
+        label1: "Primeiro Nome",
+        label2: "Segundo Nome",
+        label3: "Data de Nascimento",
+        text1: "Digite o seu primeiro nome",
+        text2: "Digite o seu segundo nome",
+        text3: "Digite a sua data de nasimento"
+    )
+    
+    let step2Content = StepContent(
+        label1: "Gênero",
+        label2: "Peso",
+        label3: "Altura",
+        text1: "Informe o seu gênero",
+        text2: "Digite o seu peso",
+        text3: "Digite a sua altura"
+    )
+    
+    let step3Content = StepContent(
+        label1: "Email",
+        label2: "Senha",
+        label3: "Confirmar Senha",
+        text1: "Digite o seu email",
+        text2: "Digite a sua senha",
+        text3: "Confirme a sua senha"
+    )
+    
+    var body: some View {
+        ZStack{
+            VStack{
+                
+            }
+            .padding(.horizontal, 24)
+        }
+    }
+}
+
+struct CreateAccountForms_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateAccountForms()
+    }
+}
